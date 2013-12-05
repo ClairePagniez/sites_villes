0eda77944cd4fe8d8cc77d0a5d91887cec6fba49
Maddyneko <neko.furuba@laposte.net>
2013-12-05 17:30:17 +0100

    Suppresion du projet

--- Novaa/tracking/utm.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if ($_GET["utm_campaign"] && !$_COOKIE["NovaaUTMCampaign"])
-		setcookie("NovaaUTMCampaign", $_GET["utm_campaign"], time()+3600, "/");
-	if ($_GET["utm_source"] && !$_COOKIE["NovaaUTMSource"])
-		setcookie("NovaaUTMSource", $_GET["utm_source"], time()+3600, "/");
-	if ($_GET["utm_medium"] && !$_COOKIE["NovaaUTMMedium"])
-		setcookie("NovaaUTMMedium", $_GET["utm_medium"], time()+3600, "/");
-	
